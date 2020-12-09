315573291824bb1fbdbb2c73dbcf732f28358f3e
문준영 <munjun-yeong@munjun-yeong-ui-MacBookPro.local>
2020-12-09 14:32:36 +0900

    큐브의 전개도에서 가운데 부분 출력해주는 기능구현

--- 과제3/work_3.py
+++ 과제3/work_3.py
@@ -7,3 +7,31 @@ class Cube:
                      [['W', 'W', 'W'], ['W', 'W', 'W'], ['W', 'W', 'W']],  # 왼쪽면 흰색 4
                      [['G', 'G', 'G'], ['G', 'G', 'G'], ['G', 'G', 'G']]]  # 오른쪽면 초록색 5
         self.Command_Count=0 # 명령어 횟수를 기록
+    
+
+    # 1. 루빅스 큐브의 전개도를 출력해주는 기능
+    def Print_Cube_Upper(self,Cube_Number): #  현재 큐브의 맨 위쪽를 출력
+        for j in range(3):
+            print("".join(self.cube[Cube_Number][0][j]),end=' ')
+    def Print_Cube_Lower(self,Cube_Number): #  현재 큐브의 맨 아래쪽를 출력
+        for j in range(3):
+            print("".join(self.cube[Cube_Number][2][j]),end=' ')
+    def Print_Cube_Center(self,Cube_Number): # 현재 큐브의 가운데를 출력
+        for j in range(3):
+            print("".join(self.cube[Cube_Number][1][j]),end=' ')
+    
+    def Print_Total_Cube_Center(self): #  큐브의 전개도에서 가운데 부분을 출력 (흰색,오렌지색,초록색,노랑색)
+        Center_Cube_Number=[4,2,5,3]
+        for i in Center_Cube_Number:
+            self.Print_Cube_Upper(i)
+            print('\t\t\t',end='')
+        print()
+        for i in Center_Cube_Number:
+            self.Print_Cube_Center(i)
+            print('\t\t\t',end='')
+        print()
+        for i in Center_Cube_Number:
+            self.Print_Cube_Lower(i)
+            print('\t\t\t',end='')
+
+    
